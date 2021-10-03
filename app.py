35c08e2863f888cdaa6da304486a763e7a3c72a3
Ambush3 <aaronbush3@gmail.com>
2021-10-03 17:03:48 -0400

    Streamlit App for CWD

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 csv_file = 'Number_of_CWD-positive_deer_identified_per_township__2015-2019_.csv'
 df = pd.read_csv(csv_file)
 
+data_clean = df
+
+data_clean.replace('Uncoded & Errors', np.nan, inplace=True)
+data_clean.replace('Unknown', np.nan, inplace=True)
+data_clean.replace('U', np.nan, inplace=True)
+data_clean.replace('None', np.nan, inplace=True)
+data_clean.replace('<null>', np.nan, inplace=True)
 
 st.title('Michigan CWD Dataset')
 st.markdown("Taking the information we can gather from the CSV file. We can create a Streamlit app to look into our "
@@ -19,23 +27,23 @@ st.sidebar.markdown('Tick a box on the side panel to explore the dataset')
 if st.sidebar.checkbox('Basic Info'):
     if st.sidebar.checkbox('Dataset Quick Look'):
         st.subheader('Dataset Quick Look: ')
-        st.write(df.head())
+        st.write(data_clean.head())
 
     if st.sidebar.checkbox('Show raw data', False):
         st.subheader('Raw data')
-        st.write(df)
+        st.write(data_clean)
 
     if st.sidebar.checkbox('Show Columns'):
         st.subheader('Show columns list')
-        all_columns = df.columns.to_list()
+        all_columns = data_clean.columns.to_list()
         st.write(all_columns)
 
     if st.sidebar.checkbox('Statistical Description'):
         st.subheader('Statistical Data Description')
-        st.write(df.describe())
+        st.write(data_clean.describe())
     if st.sidebar.checkbox('Missing Values?'):
         st.subheader('Missing values')
-        st.table(df.isnull().sum())
+        st.table(data_clean.isnull().sum())
 
 st.sidebar.title('Creating Visualizations')
 st.sidebar.subheader('Create and show visualizations.')
@@ -44,10 +52,10 @@ if st.sidebar.checkbox('Graphics'):
         st.subheader('Count Plot')
         st.info("If there's an error, adjust column name on side panel.")
         if st.checkbox('Plot the Count Plot'):
-            column_count_plot = st.sidebar.selectbox('Choose a column to plot count.', df.columns)
+            column_count_plot = st.sidebar.selectbox('Choose a column to plot count.', data_clean.columns)
             hue_opt = st.sidebar.selectbox('Optional categorical variables (countplot hue)',
-                                        df.columns.insert(0, None))
-            fig = sns.countplot(x=column_count_plot, data=df, hue=hue_opt)
+                                        data_clean.columns.insert(0, None))
+            fig = sns.countplot(x=column_count_plot, data=data_clean, hue=hue_opt)
             fig.tick_params(axis='x', rotation=90)
             st.pyplot()
 
@@ -55,20 +63,20 @@ if st.sidebar.checkbox('Graphics'):
         st.subheader('Histogram | Distplot')
         st.info("If there's an error, adjust column name on side panel.")
         if st.checkbox('Dist plot'):
-            column_dist_plot = st.sidebar.selectbox("Optional categorical variables (countplot hue)", df.columns)
-            fig = sns.displot(df[column_dist_plot])
+            column_dist_plot = st.sidebar.selectbox("Optional categorical variables (countplot hue)", data_clean.columns)
+            fig = sns.displot(data_clean[column_dist_plot])
             st.pyplot()
 
     if st.sidebar.checkbox('Boxplot'):
         st.subheader('Boxplot')
         st.info("If there's an error, adjust column name on side panel.")
         column_box_plot_X = st.sidebar.selectbox("X (Choose a column)", df.columns.insert(0, None))
-        column_box_plot_Y = st.sidebar.selectbox("Y (Choose a column - only numerical)", df.columns)
-        hue_box_opt = st.sidebar.selectbox("Optional categorical variables (boxplot hue)", df.columns.insert(0, None))
+        column_box_plot_Y = st.sidebar.selectbox("Y (Choose a column - only numerical)", data_clean.columns)
+        hue_box_opt = st.sidebar.selectbox("Optional categorical variables (boxplot hue)", data_clean.columns.insert(0, None))
         if st.checkbox('Plot Boxplot'):
             fig = sns.boxplot(x=column_box_plot_X,
                               y=column_box_plot_Y,
-                              data=df,
+                              data=data_clean,
                               palette='Set3')
             fig.tick_params(axis='x', rotation=90)
             st.pyplot()
